ad47731e621dfa57a422630aa25aa1b3673ec10d
Daniel McClean <dmcclean780@gmail.com>
2025-12-02 11:42:21 +0000

    Day 2

---
+++ day2/part2.py
@@ -0,0 +1,15 @@
+import re
+
+invalid_re = re.compile(r'^(\d+)\1+$')
+
+with open('day2/input.txt', 'r') as f:
+    line = f.readline().strip()   # read the line, remove newline
+    values = line.split(",")
+
+total = 0
+for v in values:
+    bounds = v.split("-")
+    total += sum(n 
+               for n in range(int(bounds[0]), int(bounds[1]) + 1) 
+               if invalid_re.fullmatch(str(n)))
+print(total)
